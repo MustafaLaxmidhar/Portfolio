f9b9d4fde2ea0f624b6686aa67423a532ca3406c
MustafaLaxmidhar <mufasalaxmdihar@gmail.com>
2025-04-28 13:47:10 +1200

    Gallery Updated

--- js/galleries.js
+++ js/galleries.js
@@ -1,13 +1,16 @@
 const galleries = {
-    'aus': {
-      'black': ['bees.jpg', 'citymetro.jpg', 'library.jpg', 'melbbuilding.jpg', 'skylight.jpg'],
-      'gray': ['lanterns.jpg'],
-      'other': ['redboat.jpg'],
-    },
-    'bali': {
-      'gray': ['airport.jpg', 'beachtrucks2.jpg', 'bhudda.jpg', 'buildingnight.jpg', 'catpaw.jpg', 'chinatemple.jpg', 'circlemen.jpg', 'colosseum.jpg', 'deputamadre.jpg', 'dontswim.jpg', 'dragonfly.jpg', 'DSC_1222.jpg', 'hanumanfly.jpg', 'koistatue.jpg', 'lazycat.jpg', 'luwak.jpg', 'monkeygate.jpg', 'oceantree.jpg', 'ram.jpg', 'ricefield.jpg', 'shadowcat.jpg'],
-      'black': ['batcave.jpg', 'beachtrucks1.jpg', 'bhudda (2).jpg', 'bhudda3.jpg', 'biketruck.jpg', 'bridgebus.jpg', 'brokentemple.jpg', 'butterfly.jpg', 'coconutstairs.jpg', 'crossing.jpg', 'door.jpg', 'dragonwarrior.jpg', 'dragonwarrior2.jpg', 'DSC_0011.jpg', 'DSC_0069.jpg', 'firegirl.JPG', 'girlspurple.jpg', 'hanumanfire.jpg', 'hanumansmoke.JPG', 'junglepot.jpg', 'junglestairs.jpg', 'monkeygarden.jpg', 'monkeygarden2.jpg', 'monkeyshade.jpg', 'monkeytemple.jpg', 'monkeytree.jpg', 'oceanswing.jpg', 'onimasks1.jpg', 'onimasks2.jpg', 'rockskull.jpg', 'shopalley.jpg', 'sleepycat.jpg', 'sunrays.jpg', 'temple.jpg'],
-      'other': ['dragonroof.jpg', 'DSC_0012.jpg', 'DSC_1275.jpg', 'flaminghusk.jpg'],
-      'white': ['offering.JPG', 'templepath.jpg', 'threeboats.jpg'],
-    },
-  };
+  'aus': {
+    'black': ['bees.jpg', 'citymetro.jpg', 'library.jpg', 'melbbuilding.jpg', 'skylight.jpg'],
+    'gray': ['lanterns.jpg'],
+    'green': ['redboat.jpg'],
+  },
+  'bali': {
+    'gray': ['airport.jpg', 'beachtrucks2.jpg', 'beachtrucks1.jpg', 'bhudda.jpg', 'catpaw.jpg', 'chinatemple.jpg', 'circlemen.jpg', 'colosseum.jpg', 'deputamadre.jpg', 'dontswim.jpg', 'dragonfly.jpg', 'dragonroof.jpg', 'DSC_1222.jpg', 'hanumanfly.jpg', 'koistatue.jpg', 'lazycat.jpg', 'luwak.jpg', 'monkeygate.jpg', 'oceantree.jpg', 'ram.jpg', 'ricefield.jpg', 'shadowcat.jpg'],
+    'black': ['batcave.jpg', 'bhudda (2).jpg', 'bhudda3.jpg', 'biketruck.jpg', 'bridgebus.jpg', 'brokentemple.jpg', 'coconutstairs.jpg', 'crossing.jpg', 'door.jpg', 'dragonwarrior.jpg', 'dragonwarrior2.jpg', 'DSC_0011.jpg', 'DSC_0069.jpg', 'firegirl.JPG', 'hanumanfire.jpg', 'hanumansmoke.JPG', 'junglepot.jpg', 'junglestairs.jpg', 'monkeygarden.jpg', 'monkeygarden2.jpg', 'monkeyshade.jpg', 'monkeytemple.jpg', 'monkeytree.jpg', 'oceanswing.jpg', 'rockskull.jpg', 'shopalley.jpg', 'sleepycat.jpg', 'sunrays.jpg'],
+    'blue': ['buildingnight.jpg'],
+    'green': ['butterfly.jpg', 'girlspurple.jpg', 'onimasks1.jpg', 'onimasks2.jpg', 'temple.jpg'],
+    'red': ['DSC_0012.jpg', 'DSC_1275.jpg'],
+    'orange': ['flaminghusk.jpg'],
+    'white': ['offering.JPG', 'templepath.jpg', 'threeboats.jpg'],
+  },
+};
